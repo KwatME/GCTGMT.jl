0f46fb16b3418b1e8303b6bc7ee8ac0f8d5262f9
KwatME <kwat.me@icloud.com>
2021-04-12 16:38:26 -0700

    2

--- src/GCTGMT.jl
+++ src/GCTGMT.jl
@@ -1,6 +1,7 @@
 module GCTGMT
 
 include("read_gct.jl")
+
 include("read_gmt.jl")
 
 end
--- src/read_gmt.jl
+++ src/read_gmt.jl
@@ -1,35 +1,52 @@
 function read_gmt(path::String)::Dict{String, Vector{String}}
 
+
     gene_set_to_gene_ = Dict{String, Vector{String}}()
+
 
     for line in readlines(path)
 
+
         split_ = split(line, '\t')
+
 
         gene_set = split_[1]
 
+
         gene_ = split_[3:end]
+
 
         gene_set_to_gene_[gene_set] = gene_
 
+
     end
+
 
     return gene_set_to_gene_
 
+
 end
+
 
 function read_gmt(path_::Vector{String})::Dict{String, Vector{String}}
 
+
     gene_set_to_gene_ = Dict{String, Vector{String}}()
+
 
     for path in path_
 
+
         merge!(gene_set_to_gene_, read_gmt(path))
+
 
     end
 
+
     return gene_set_to_gene_
 
+
 end
+
 
 export read_gmt
