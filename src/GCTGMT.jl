c4c6a767bf9d81e04d2a11ea96ab49fe2ba0e0ba
KwatME <kwat.me@icloud.com>
2021-04-12 21:54:22 -0700

    2 lines

--- src/GCTGMT.jl
+++ src/GCTGMT.jl
@@ -1,7 +1,10 @@
 module GCTGMT
 
+
 include("read_gct.jl")
 
+
 include("read_gmt.jl")
+
 
 end
--- src/read_gct.jl
+++ src/read_gct.jl
@@ -1,16 +1,23 @@
 using CSV: File
 using DataFrames: DataFrame, Not, rename!, select!
 
+
 function read_gct(path::String; axis_0_name::String = "Axis 0 Name")::DataFrame
+
 
     df = DataFrame(File(path; header = 3, delim = '\t'))
 
+
     select!(df, Not(Symbol("Description")))
+
 
     rename!(df, Symbol("Name") => Symbol(axis_0_name))
 
+
     return df
 
+
 end
+
 
 export read_gct
